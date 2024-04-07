3a34f26f5cc518bf5a81ccef7193230a76ccc1dc
Muhammad Muzamil <165401047+Muzammmmmmil@users.noreply.github.com>
2024-04-07 13:48:01 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const cont =document.querySelector(".container");
+        const tex =document.querySelector(".text");
+    const quest = document.querySelector(".question");
+const yes = document.querySelector(".yes");
+const noo = document.querySelector(".no");
+const gif = document.querySelector(".gif");
+
+yes.addEventListener("click", () => {
+    quest.innerHTML="I LIKE YOU TO";
+    gif.src= "I-Love-You-Hug-GIF-by-Chubbive-unscreen.gif";
+    gif.style.height ="300px"; 
+    gif.style.width ="300px"; 
+  
+    gif.style.borderradius ="12px"; 
+    quest.style.fontSize= "52px";
+    quest.style.fontFamily= "DM Serif Display";
+   
+    
+});
+gif.addEventListener("click", () => {
+    tex.innerHTML="I LOVE YOU ";
+});
+
+noo.addEventListener("mouseover", ()=> {
+    const nooRect = noo.getBoundingClientRect();
+    const maxw = window.innerWidth - nooRect.width;
+    const maxh =window.innerHeight - nooRect.height;
+
+    const x = Math.floor(Math.random()*maxw);
+    const y = Math.floor(Math.random()*maxh);
+
+    noo.style.left = x + "px";
+    noo.style.top = y + "px";
+})
